5016c65dbfc69529b573071e3ecce673d972819e
remvil <rvildacci@gmail.com>
2025-11-27 13:06:26 +0100

    added swagger page

--- backend/src/events/events.controller.ts
+++ backend/src/events/events.controller.ts
@@ -1,27 +1,35 @@
 import {Controller, Get, Post, Put, Delete, Body, Param} from "@nestjs/common";
 import {EventsService} from "./events.service";
 import {EventDto} from "./dto";
+import { ApiTags, ApiResponse, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('events')
 @Controller("events")
 export class EventsController {
 	constructor(private readonly eventsService: EventsService) {}
 
 	@Get()
+	@ApiResponse({ status: 200, description: 'List events' })
 	findAll() {
 		return this.eventsService.findAll();
 	}
 
 	@Post()
+	@ApiBody({ type: EventDto })
+	@ApiResponse({ status: 201, description: 'Event created' })
 	create(@Body() eventData: EventDto) {
 		return this.eventsService.create(eventData);
 	}
 
 	@Put(":id")
+	@ApiBody({ type: EventDto })
+	@ApiResponse({ status: 200, description: 'Event updated' })
 	update(@Param("id") id: string, @Body() eventData: EventDto) {
 		return this.eventsService.update(id, eventData);
 	}
 
 	@Delete(":id")
+	@ApiResponse({ status: 200, description: 'Event deleted' })
 	delete(@Param("id") id: string) {
 		return this.eventsService.delete(id);
 	}
